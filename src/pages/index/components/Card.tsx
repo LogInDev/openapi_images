f6734260b0b63b4c06e270190fea474a7ca3c4e5
LogInDev <fhazlt303@gmail.com>
2025-03-01 19:17:20 +0900

    14강 type error-imgSelecto(index.tsx)

--- src/pages/index/components/Card.tsx
+++ src/pages/index/components/Card.tsx
@@ -1,13 +1,18 @@
+import { CardDTO } from '../types/card'
 import styles from './Card.module.scss'
 
-function Card() {
+interface Props {
+    data: CardDTO
+}
+
+function Card({data}:Props) {
     const openDialog = () => {
         console.log('open dialog')
     }
 
     return (
         <div className={styles.card} onClick={openDialog}>
-            <img src="" className={styles.card__image} />
+            <img src={data.urls.small} alt={data.alt_description} className={styles.card__image} />
         </div>
     )
 }
